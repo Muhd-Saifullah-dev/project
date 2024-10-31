4a2b8bd5303c513ba51074317fd948e4be78c680
Muhd-Saifullah-dev <saifullahdev98@gmail.com>
2024-10-31 19:21:41 +0500

    Error handling add

---
+++ src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class ApiError extends Error{
+    constructor(message="Something Went wrong !",statusCode,errors=[],stack=''){
+        super(message)
+            this.message=message
+          
+            this.sucess=false
+            this.data=null
+            this.errors=errors
+            this.statusCode=statusCode
+
+            if(stack){
+                this.stack=stack
+            }
+            else{
+                Error.captureStackTrace(this,this.constructor)
+            }
+    }
+  
+  
+}
+
+export {ApiError}
